5dba30cd5426615d180b4fc4fedb5ff4ce1d9aa4
lauraaa05 <laushp01@easv365.dk>
2026-03-09 11:17:08 +0100

    please status work thanks

--- client/src/components/ChartsPanel.tsx
+++ client/src/components/ChartsPanel.tsx
@@ -1,8 +1,12 @@
-const ChartsPanel = () => {
+interface Props {
+    selectedTurbineId: string;
+}
+
+const ChartsPanel = ({selectedTurbineId}: Props) => {
     return (
         <div className="bg-slate-800 border border-slate-800 rounded-xl p-6">
             <h2 className="text-sm mb-4 text-slate-400 uppercase">
-                Telemetry
+                Telemetry for {selectedTurbineId}
             </h2>
 
             <div className="grid grid-cols-2 gap-6">
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,9 +3,25 @@ import TurbineList from "../components/TurbineList";
 import ChartsPanel from "../components/ChartsPanel";
 import ControlsPanel from "../components/ControlsPanel";
 import AlertsPanel from "../components/AlertsPanel";
+import {useState} from "react";
+
+interface Turbine {
+    id: string;
+    name: string;
+}
 
 const Dashboard = () => {
     const { logout, user } = useAuth();
+    
+    const [turbineList] = useState<Turbine[]>([
+        { id: "turbine-alpha", name: "Alpha"},
+        { id: "turbine-beta", name: "Beta"},
+        { id: "turbine-gamma", name: "Gamma"},
+        { id: "turbine-delta", name: "Delta"}
+    ]);
+    const [selectedTurbineId, setSelectedTurbineId] = useState<string>("turbine-alpha");
+    
+    
 
     return (
         <div className="min-h-screen bg-slate-900 text-slate-200">
@@ -28,16 +44,22 @@ const Dashboard = () => {
 
             <div className="grid grid-cols-12 gap-6 p-6">
                 <div className="col-span-3">
-                    <TurbineList />
+                    <TurbineList 
+                    turbines={turbineList}
+                    selectedTurbineId={selectedTurbineId}
+                    onChangeTurbine={setSelectedTurbineId}/>
                 </div>
 
                 <div className="col-span-6">
-                    <ChartsPanel />
+                    <ChartsPanel selectedTurbineId={selectedTurbineId}/>
                 </div>
 
                 <div className="col-span-3 space-y-6">
-                    <ControlsPanel />
-                    <AlertsPanel />
+                    <ControlsPanel 
+                    turbines={turbineList}
+                    selectedTurbineId={selectedTurbineId}
+                    onChangeTurbine={setSelectedTurbineId}/>
+                    <AlertsPanel selectedTurbineId={selectedTurbineId}/>
                 </div>
             </div>
         </div>
